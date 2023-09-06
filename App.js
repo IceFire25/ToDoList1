016406e645e4c70be6d7c52f326565e53ad05a73
IceFire25 <alinai-play@mail.ru>
2023-09-06 16:26:24 +0300

    master

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Todo list:</Text>
+      <Text style={styles.text}>1. To do smth {"\n"} 2. Profit!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +17,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: "blue",
   },
 });
