9640830f007c9c18af908a0201b969b76111c640
IceFire25 <alinai-play@mail.ru>
2023-09-15 13:29:40 +0300

    master

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Header } from './components/header';
 
 export default function App() {
+    const [listOfItems, setListOfItems] = useState([
+        { text: "Купить ...", key: "1"},
+        { text: "Помыть ...", key: "2"},
+        { text: "Сделать ...", key: "3"},
+    ]);
   return (
-    <Text>Create</Text>
+    <View style={styles.header}>
+    <Header/>
+        <View>
+            <FlatList
+            data={listOfItems}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+            />
+        </View>
+    </View>
   );
 }
 
@@ -14,5 +28,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    flex: 1,
   },
 });
