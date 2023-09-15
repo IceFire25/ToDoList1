a07664551b4281a5ca7af3bfd11d03f09f532dca
IceFire25 <alinai-play@mail.ru>
2023-09-15 13:53:42 +0300

    master

--- App.js
+++ App.js
@@ -25,10 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    fontSize: 30,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   header: {
     flex: 1,
